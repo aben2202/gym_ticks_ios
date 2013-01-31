6d40cf6d789e431c1aa96a2c1d5c2cbce60f69ee
Andrew Benson <andebenson@gmail.com>
2013-01-31 10:10:32 -0600

    Initial Commit

---
+++ top_this/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  top_this
+//
+//  Created by Andrew Benson on 1/31/13.
+//  Copyright (c) 2013 Andrew Benson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
